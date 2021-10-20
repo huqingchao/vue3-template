5604a8860da4c8afaf98bb9cd40e153c95742de4
huqc <huqc@antaiib.com>
2021-10-20 17:31:33 +0800

    test husky

---
+++ src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'Test',
+    data() {
+      return {
+        a:1
+      };
+    },
+    methods: {},
+  };
+</script>
+
+<style lang="less" scoped></style>
