a5cdb558029bc701b9866a2b35efa9f0fc3760c4
huqingchao <1026489907@qq.com>
2022-05-12 11:05:37 +0800

    test husky

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,21 @@
 <script setup lang="ts">
+  import { useMainStore, fun } from '/@/store/main';
+  import { storeToRefs } from 'pinia';
   import { ref } from 'vue';
+
+  const mainStore = useMainStore();
 
   defineProps<{ msg: string }>();
 
   const count = ref(0);
+  const { name} = storeToRefs(mainStore);
+  name.value = 'aa';
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>pinia:{{ name }}</div>
 </template>
 
 <style scoped>
