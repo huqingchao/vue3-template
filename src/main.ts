664cbd28b918fab4f5961d159ee8d892ec4eb181
huqingchao <1026489907@qq.com>
2022-05-18 14:26:59 +0800

    feat: test husky

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from '/@/router';
+const a = 1;
 createApp(App).use(router).use(createPinia()).mount('#app');
